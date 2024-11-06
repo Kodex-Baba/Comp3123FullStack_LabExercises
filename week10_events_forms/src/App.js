24d6ab8509f1763accdca7b34cb600b0c64fda93
Kodex-Baba <122707428+Kodex-Baba@users.noreply.github.com>
2024-11-05 20:18:30 -0500

    Week 10 done

---
+++ week10_events_forms/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login.js';
+import Signup from './components/SignUp.js';
+
+function App() {
+  return (
+    <div>
+      <h1>Events and Forms</h1>
+        <Login></Login>
+        <Signup></Signup>
+    </div>
+  );
+}
+
+export default App;
